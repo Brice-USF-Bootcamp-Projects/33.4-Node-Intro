c40079536fa45501e7ad1b4698225957d6d4b545
Brice Nelson <bnelsonemail@icloud.com>
2025-01-05 13:29:36 -0500

    added logic for step 2 & 3

--- step2.js
+++ step2.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
 const process = require('process')
+const axios = require('axios')
 
 
 // fs.readFile('one.txt', 'utf-8', (err, data) => {
@@ -20,6 +21,26 @@ const cat = (path) => {
       }
     });
   }
-  
-  cat(process.argv[2]);
 
+
+
+
+/** read page at URL and print it out. */
+
+async function webCat(url) {
+    try {
+      let resp = await axios.get(url);
+      console.log(resp.data);
+    } catch (err) {
+      console.error(`Error fetching ${url}: ${err}`);
+      process.exit(1);
+    }
+  }
+  
+  let path = process.argv[2];
+  
+  if (path.slice(0, 4) === 'http') {
+    webCat(path);
+  } else {
+    cat(path);
+  }
